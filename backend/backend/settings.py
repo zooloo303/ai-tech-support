e8f2b8bc8358659fb3920dd0d670cd884f47fcd0
Neil Ward <nward@dassian.com>
2024-04-19 19:36:28 -0700

    sorting deployment issues

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-(3oo_sj0%arx4j%)cn^y$x71mhg-=p!giwo%3d9rz3_62fo*jm'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
